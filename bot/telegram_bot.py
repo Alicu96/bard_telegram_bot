923c2e621fc297a208b5220c2def6a479dab4d55
Ho Minh Tri <trihm23@fpt.com.vn>
2023-05-17 15:18:27 +0700

    add check heartbeat

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1,7 +1,7 @@
 import telebot
 import dotenv
 import os
-import bardapi
+from bardapi import Bard
 
 dotenv.load_dotenv()
 
@@ -9,14 +9,23 @@ telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
 bot = telebot.TeleBot(telegram_bot_token)
 
+bard = Bard(timeout=10) # Set timeout in seconds
+
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, "Hello! I am a bot that replies to your chat.")
 
+
+@bot.message_handler(commands=["heartbeat"])
+def heartbeat(message):
+    bot.send_message(message.chat.id, "I am still alive!")
+
+
 @bot.message_handler(content_types=["text"])
 def reply(message):
     query = message.text
-    response = bardapi.core.Bard().get_answer(query)['content']
+    response = bard.get_answer(query)['content']
     bot.send_message(message.chat.id, response)
+
 
 bot.polling()
